94d9c5453b710e251ec1bebf109e438fdaf0480e
Jose Maria Morcuende Jiménez <josemarimj@gmail.com>
2023-10-28 14:40:03 +0200

    LocalDateTimeAdapter

---
+++ src/main/java/cat/xtect/ioc/domain/LocalDateTimeAdapter.java
@@ -0,0 +1,18 @@
+package cat.xtect.ioc.domain;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
+
+    @Override
+    public LocalDateTime unmarshal(String v) throws Exception {
+        return LocalDateTime.parse(v, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+
+    @Override
+    public String marshal(LocalDateTime v) throws Exception {
+        return v.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+}
