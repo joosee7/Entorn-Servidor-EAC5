3c63afcbf02521137b3e8fe131ba0b5ebcffd236
Jose Maria Morcuende Jiménez <josemarimj@gmail.com>
2023-10-28 15:32:32 +0200

    Dentista class

---
+++ src/main/java/cat/xtect/ioc/domain/Dentista.java
@@ -0,0 +1,59 @@
+package cat.xtect.ioc.domain;
+
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.LocalDateTime;
+
+public class Dentista {
+    private String codi;
+    private Double pacientAcumulat;
+    private Double preuPeces;
+    private Boolean pacientActual;
+    private LocalDateTime datePacient;
+
+    public Dentista(String codi) {
+        this.codi = codi;
+    }
+
+    public void setCodi(String codi) {
+        this.codi = codi;
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public void setDatePacient(LocalDateTime datePacient) {
+        this.datePacient = datePacient;
+    }
+
+    @XmlJavaTypeAdapter(LocalDateTimeAdapter.class)
+    @XmlSchemaType(name = "dateTime")
+    public LocalDateTime getDatePacient() {
+        return datePacient;
+    }
+
+    public void setPacientActual(Boolean pacientActual) {
+        this.pacientActual = pacientActual;
+    }
+
+    public Boolean getPacientActual() {
+        return pacientActual;
+    }
+
+    public void setPacientAcumulat(Double pacientAcumulat) {
+        this.pacientAcumulat = pacientAcumulat;
+    }
+
+    public Double getPacientAcumulat() {
+        return pacientAcumulat;
+    }
+
+    public void setPreuPeces(Double preuPeces) {
+        this.preuPeces = preuPeces;
+    }
+
+    public Double getPreuPeces() {
+        return preuPeces;
+    }
+}
